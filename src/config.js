39d04f775d3d7270326cc8e5cc3a8ca6d19c2e01
Deep Saha <dip23447@gmail.com>
2026-04-26 19:51:34 +0530

    Passed file through command line

--- src/config.js
+++ src/config.js
@@ -5,13 +5,12 @@ import {rootConfigSchema} from './configSchema.js';
 // READ CONFIGURATION 
 export async function parseYamlConfig(filePath){
     const configFileContent = await fs.readFile(filePath, 'utf8');
-
     const configParsed = parse(configFileContent);
-    return JSON.stringify(configParsed);
+    return configParsed;
 
 }
 
 export async function validateConfig(config){
-    const validatedConfig = await rootConfigSchema.parseAsync(JSON.parse(config));
-    return validateConfig;
+    const validatedConfig = await rootConfigSchema.parseAsync(config);
+    return validatedConfig;
 }
--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-console.log("Hello")
+import { program } from 'commander';
+import path from 'node:path';
+import { parseYamlConfig, validateConfig } from './config.js';
+
+async function main() {
+    program.option('--config <path>', 'Path to config file');
+
+    program.parse(); // cleaner
+
+    const options = program.opts();
+
+    if (!options.config) {
+        console.error("❌ Please provide --config");
+        process.exit(1);
+    }
+
+    const configPath = path.resolve(process.cwd(), options.config);
+
+    console.log("Using config:", configPath);
+
+    try {
+        const parsedConfig = await parseYamlConfig(configPath);
+        const validatedConfig = await validateConfig(parsedConfig);
+
+        console.log("Valid Config:");
+        console.dir(validatedConfig, { depth: null });
+
+    } catch (err) {
+        console.error("Error:", err.message);
+    }
+}
+
+main();
